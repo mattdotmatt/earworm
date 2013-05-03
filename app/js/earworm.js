7eaf76a99640eb8e5b4ebdbfb6cda7c5024f883b
Matt Young <mpyoung@gmail.com>
2013-05-03 13:58:41 +0100

    Added basic jasmine testing

---
+++ app/js/earworm.js
@@ -0,0 +1,7 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: myoung
+ * Date: 03/05/13
+ * Time: 13:48
+ * To change this template use File | Settings | File Templates.
+ */
---
+++ tests/earworms.tests.js
@@ -0,0 +1,12 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: myoung
+ * Date: 03/05/13
+ * Time: 13:27
+ * To change this template use File | Settings | File Templates.
+ */
+describe("A suite", function() {
+    it("contains spec with an expectation", function() {
+        expect(true).toBe(true);
+    });
+});
