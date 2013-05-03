9e5604f85b95d7a374c875705e24af95f53708ea
Matt Young <mpyoung@gmail.com>
2013-05-04 00:12:39 +0100

    Added angular

--- app/js/earworm.js
+++ app/js/earworm.js
@@ -5,3 +5,12 @@
  * Time: 13:48
  * To change this template use File | Settings | File Templates.
  */
+var EarWorm = {
+    getSearchDateRange: function (birthDay) {
+        "use strict";
+        return '';
+    }
+};
+
+
+
--- tests/earworms.tests.js
+++ tests/earworms.tests.js
@@ -1,12 +1,14 @@
 /**
- * Created with JetBrains WebStorm.
  * User: myoung
  * Date: 03/05/13
  * Time: 13:27
- * To change this template use File | Settings | File Templates.
  */
-describe("A suite", function() {
-    it("contains spec with an expectation", function() {
-        expect(true).toBe(true);
+
+describe("Earworms", function () {
+    "use strict";
+    it("uses a date range based around the birthday", function() {
+        var birthday = new Date(2010, 12, 23);
+        var x = EarWorm.getSearchDateRange(birthday);
+        expect(x).toBe('hello');
     });
 });
